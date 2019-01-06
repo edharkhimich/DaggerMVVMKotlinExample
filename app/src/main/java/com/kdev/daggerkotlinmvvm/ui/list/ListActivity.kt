aecb94e5de193533b8b1e86c9f30fb967b55586d
Edgar Khimich <edgarkhimich@gmail.com>
2019-01-05 23:07:22 -0500

    first commit/ add dependencies

---
+++ app/src/main/java/com/kdev/daggerkotlinmvvm/ui/list/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.kdev.daggerkotlinmvvm.ui.list
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kdev.daggerkotlinmvvm.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
